bc6fa8cf9eccc7086bc51e569ac7311fdab3904c
Peyton Lyons <peyton.lyons@outlook.com>
2022-11-11 23:12:38 -0500

    Initial Commit

---
+++ mixer/mixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixerApp.swift
+//  mixer
+//
+//  Created by Peyton Lyons on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct mixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
